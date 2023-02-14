f7a68d27a7d58051373c74d3325ce4dcbda46af2
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-02-14 17:34:06 +0100

    FIX comments delete

--- js/app.js
+++ js/app.js
@@ -56,9 +56,9 @@ for (let i=0; i< membriTeam.length;i++){
     const roleMember = member.ruolo
     const pictureMember = member.foto
     memberEl.innerHTML+= `<div class="col-4 bg-white p-0 m-2 mc-adaptation"> 
-    <img style="width:100% " src="${pictureMember}" alt="">
-    <p class="member text-center">${nameMember}</p> 
-    <p class="role text-center">${roleMember}</p>
+    ${pictureMember},
+   ${nameMember}, 
+   ${roleMember}
     </div>`
 }
 
