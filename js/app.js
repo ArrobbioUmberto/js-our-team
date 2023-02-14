b78c22f5a596d36e403f5f934d048deb431b5fdc
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-02-14 15:29:58 +0100

    FEAT milestone 0- 1

--- js/app.js
+++ js/app.js
@@ -1,3 +1,48 @@
 console.log('ciao')
 
+// Wayne Barnett	Founder & CEO	wayne-barnett-founder-ceo.jpg
+// Angela Caroll	Chief Editor	angela-caroll-chief-editor.jpg
+// Walter Gordon	Office Manager	walter-gordon-office-manager.jpg
+// Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
+// Scott Estrada	Developer	scott-estrada-developer.jpg
+// Barbara Ramos	Graphic Designer	barbara-ramos-graphic-designer.jpg
 
+const membroTeam = {
+    nome: 'Wayne Barnett',
+    ruolo: 'Founder & CEO',
+    foto: './img/wayne-barnett-founder-ceo.jpg',
+}
+
+const membriTeam = [
+    membroTeam,
+    {
+        nome: 'Angela Caroll',
+        ruolo: 'Chief Editor',
+        foto: './img/angela-caroll-chief-editor.jpg',
+    },
+    {
+        nome: 'Walter Gordon',
+        ruolo: 'Office Manager',
+        foto: './img/walter-gordon-office-manager.jpg',
+    }, 
+    {
+        nome: 'Angela Lopez',
+        ruolo: 'Social Media Manager',
+        foto: './img/angela-lopez-social-media-manager.jpg',
+    }, 
+    {
+        nome: 'Scott Estrada',
+        ruolo: 'Developer',
+        foto: './img/scott-estrada-developer.jpg',
+    }, 
+    {
+        nome: 'Barbara Ramos',
+        ruolo: 'Graphic Designer',
+        foto: './img/barbara-ramos-graphic-designer.jpg',
+    }
+]
+
+
+for (let key in membriTeam) {
+    console.log(membriTeam[key])
+}
